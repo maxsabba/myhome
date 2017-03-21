55af81a15fc5c61d1fa57ae04abb4aa536a10320
BuildTools <unconfigured@null.spigotmc.org>
2017-03-21 14:33:56 +0100

    First commit

---
+++ arduino/dbtest.py
@@ -0,0 +1,23 @@
+import sqlite3 as lite
+import sys
+
+
+con = None
+
+
+try:
+    con = lite.connect('./database/myhome.db')
+
+    cur = con.cursor()
+    cur.execute('SELECT SQLITE_VERSION()')
+    data = cur.fetchone()
+    print "SQLite version: %s" % data
+
+except lite.Error, e:
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+
+
+finally:
+    if con:
+        con.close()
---
+++ arduino/tempreader.py
@@ -0,0 +1,35 @@
+# import Serial
+import sqlite3
+import sys
+from datetime import datetime
+
+# set serial inteface to read data from arduinoSerialData
+# arduinoSerialData = serial.Serial('/dev/ttyACM0',9600)
+
+while 1:
+    if(arduinoSerialData.inWaiting()&gt;0):
+    myData = arduinoSerialData.readline()
+    print myData
+
+con = None
+
+try:
+    con = sqlite3.connect('./database/myhome.db')
+    cur = con.cursor()
+    # add the selcet query
+    # today = str(datetime.now())
+    # print today
+    cur.execute("INSERT INTO temperature (readingtime,arduino_id,tempvalue) \
+    VALUES (?, ?, ?);", (str(datetime.now()), "Stanza1", 20))
+    data = cur.fetchone()
+    print "SQLite messagge: %s" % data
+    con.commit()
+
+
+except sqlite3.Error, e:
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+
+finally:
+    if con:
+        con.close()
