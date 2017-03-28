80c1a04d90a6aee79a68c8e3222ab850cdce4bed
maxsabba <maxsabba@gmail.com>
2017-03-28 09:07:43 +0200

    Fix some bug

--- arduino/tempreader.py
+++ arduino/tempreader.py
@@ -1,32 +1,43 @@
+from __future__ import print_function
 import serial
-import sqlite3
+import sqlite3 as lite
+import time
 import sys
 from datetime import datetime
 
-# set serial inteface to read data from arduinoSerialData
-arduinoSerialData = serial.Serial('/dev/ttyACM0', 9600)
-
-while 1:
-    myData = arduinoSerialData.readline()
-
 
 con = None
+count = 0
+
+
+def readfromserial():
+    # set serial inteface to read data from arduinoSerialData
+    # arduinoSerialData = serial.Serial('/dev/ttyACM0', 9600)
+    # myData = arduinoSerialData.readline()
+    myData = "Stanza1; 30.9"
+    print('Ciclo while')
+    return myData
+
 
 try:
-    con = lite.connect('../database/myhome.db')
-    cur = con.cursor()
-    # add the selcet query
-    # today = str(datetime.now())
-    print myData
-    cur.execute("INSERT INTO temperature (readingtime,arduino_id,tempvalue) \
-    VALUES (?, ?, ?);",(str(datetime.now()), myData.split(';')[0], myData.split(';')[1]))
-    data = cur.fetchone()
-    print "SQLite messagge: %s" % data
-    con.commit()
+    while (count < 10):
+        con = lite.connect('../database/myhome.db')
+        cur = con.cursor()
+        # add the selcet query
+        # today = str(datetime.now())
+        fromSer = readfromserial()
+        print(fromSer)
+        cur.execute("INSERT INTO temperature (readingtime,arduino_id,tempvalue) \
+        VALUES (?, ?, ?);", (str(datetime.now()), fromSer.split(';')[0], fromSer.split(';')[1]))
+        data = cur.fetchone()
+        print("SQLite messagge: %s" % data)
+        con.commit()
+        time.sleep(10)
+        count = count + 1
 
 
-except sqlite3.Error, e:
-    print "Error %s:" % e.args[0]
+except lite.Error, e:
+    print("Error %s:" % e.args[0])
     sys.exit(1)
 
 finally:
